bf83fc146e5cdbf26b38753c832ad1850ce8ec86
Jinny <dms873@naver.com>
2022-06-14 14:15:55 +0900

    delete home, update pom, root-context, add err
    - home.jsp, HomeController 삭제
    - pom.xml : gson 추가
    - root-context.xml : transction 코드 <tx>추가
    - MainController에 logger, ExceptionHandler 추가
    - error페이지 추가

--- bab_project/src/main/java/kh/spring/bab/main/controller/MainController.java
+++ bab_project/src/main/java/kh/spring/bab/main/controller/MainController.java
@@ -1,7 +1,10 @@
 package kh.spring.bab.main.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -9,10 +12,22 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping("main")
 public class MainController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
 
 	@GetMapping("/")
 	public ModelAndView select(ModelAndView mv) {
 		mv.setViewName("main/mainpage");
+		return mv;
+	}
+	
+	@ExceptionHandler(Exception.class)
+	private ModelAndView handlerMemberException(Exception e) {
+		logger.error(e.getMessage());
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("errMsg", e.getMessage());
+		// 에러페이지로 이동하는걸 추천
+		mv.setViewName("error/errException");
 		return mv;
 	}
 	
